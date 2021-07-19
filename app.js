11bae5e96a23e786e53bcbcbeed431cc43509d3e
Mustafa Cagatay Onal <mustafacagatayonal@users.noreply.github.com>
2021-07-19 13:29:29 +0300

    second commit

--- app.js
+++ app.js
@@ -11,3 +11,10 @@
 //manual approach (create package.json in the root, create properties etc)
 //npm init (step by step, press enter to skip)
 //npm init -y (everything default)
+
+//git init
+//git add .
+//git commit -m "first commit"
+//git remote add origin https://github.com/mustafaonal/temp-node.git
+//git branch -M main
+//git push -u origin main
